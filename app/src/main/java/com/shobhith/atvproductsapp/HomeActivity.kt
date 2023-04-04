ba4161025b20cf42ff245efba68775ae3a302810
shobith <shobith@robosoftin.com>
2023-04-04 15:12:25 +0530

    Initial commit

---
+++ app/src/main/java/com/shobhith/atvproductsapp/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.shobhith.atvproductsapp
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class HomeActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
