5a8dd144f87b5134e397e6af306e37a7c3e8c9e2
shobith <shobith@robosoftin.com>
2023-04-04 19:55:09 +0530

    Adding required dependencies

--- app/src/main/java/com/shobhith/atvproductsapp/home/presentation/ui/HomeActivity.kt
+++ app/src/main/java/com/shobhith/atvproductsapp/home/presentation/ui/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.shobhith.atvproductsapp
+package com.shobhith.atvproductsapp.home.presentation.ui
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.shobhith.atvproductsapp.R
 
 class HomeActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
