ac471d73932bcef73bb4c094e6b3166e86ad8bb2
shobith <shobith@robosoftin.com>
2023-04-05 10:27:58 +0530

    Koin Setup

---
+++ app/src/main/java/com/shobhith/atvproductsapp/common/ui/ProductsApplication.kt
@@ -0,0 +1,16 @@
+package com.shobhith.atvproductsapp.common.ui
+
+import android.app.Application
+import com.shobhith.atvproductsapp.home.di.homeModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ProductsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ProductsApplication)
+            modules(homeModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/shobhith/atvproductsapp/home/di/HomeModule.kt
@@ -0,0 +1,7 @@
+package com.shobhith.atvproductsapp.home.di
+
+import org.koin.dsl.module
+
+val homeModule = module {
+
+}
